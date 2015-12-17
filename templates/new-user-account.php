4e82e50237f995b9a4ab7f79306453ce41f00d9b
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2015-12-17 06:14:42 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- templates/new-user-account.php
+++ templates/new-user-account.php
@@ -13,8 +13,8 @@ if ( ! defined('ABSPATH')) {
 
 do_action('buddypress_welcome_email_header', $email_heading);
 
-echo apply_filters('buddypress_welcome_email_text', '<p>' . sprintf(__("Welcome to %s. We thank you for joining the site.<br><br>Your username is <strong>%s</strong>.<br>Your password is <strong>%s</strong>.", 'buddypress-welcome-email'), esc_html($blogname), esc_html($user_login), esc_html($user_pass))) . '</p>';
+echo apply_filters('buddypress_welcome_email_text', '<p>'.sprintf(__("Welcome to %s. We thank you for joining the site.<br><br>Your username is <strong>%s</strong>.<br>Your password is <strong>%s</strong>.", 'buddypress-welcome-email'), esc_html($blogname), esc_html($user_login), esc_html($user_pass))).'</p>';
 
-echo apply_filters('buddypress_welcome_email_footer_text', '<p>' . __('Regards,', 'buddypress-welcome-email') . "</p>\n<p>" . $blogname . "</p>\n");
+echo apply_filters('buddypress_welcome_email_footer_text', '<p>'.__('Regards,', 'buddypress-welcome-email')."</p>\n<p>".$blogname."</p>\n");
 
 do_action('buddypress_welcome_email_footer');
